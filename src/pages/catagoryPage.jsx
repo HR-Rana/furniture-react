8c2edb0d6729cc8d199eb05583d3cef281d4191e
Rana <ariyanmahmud02019@gmail.com>
2024-11-03 00:10:23 +0600

    user router updated

---
+++ src/pages/catagoryPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function catagoryPage() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      color:{
+        'primary': '#3498db',
+        'light-gray': '#f8f9fa',
+       'medium-gray': '#ced4da',
+      }
+    },
   },
   plugins: [],
 }
